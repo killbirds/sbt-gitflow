ac1f0f6c607c90478973a528abbdf5800424c9b2
killbirds <killbirds@gmail.com>
2017-11-05 10:11:11 +0900

    minor patch

--- src/main/scala/com/killbirds/sbt/sbtrelease/gitflow/Steps.scala
+++ src/main/scala/com/killbirds/sbt/sbtrelease/gitflow/Steps.scala
@@ -4,7 +4,6 @@ import sbt._
 import sbtrelease._
 import ReleasePlugin.autoImport.ReleaseKeys.versions
 import ReleasePlugin.autoImport.releaseVcs
-import ReleasePlugin.autoImport.ReleaseStep
 
 object Steps {
   private def releaseVersion(state: State): Either[String, String] = {
@@ -21,22 +20,24 @@ object Steps {
     }
   }
 
-  private def run(fn: GitFlow => Either[String, Unit]): ReleaseStep = ReleaseStep({ state =>
+  private type ReleaseStep = State => State
+
+  private def run(fn: GitFlow => Either[String, Unit]): ReleaseStep = { state =>
     (for {
       git <- getGit(state)
       _ <- fn(GitFlow(git))
     } yield ()).swap.foreach(sys.error)
     state
-  })
+  }
 
-  private def run(fn: (GitFlow, String) => Either[String, Unit]): ReleaseStep = ReleaseStep({ state =>
+  private def run(fn: (GitFlow, String) => Either[String, Unit]): ReleaseStep = { state =>
     (for {
       git <- getGit(state)
       version <- releaseVersion(state)
       _ <- fn(GitFlow(git), version)
     } yield ()).swap.foreach(sys.error)
     state
-  })
+  }
 
   val checkGitFlowExists: ReleaseStep = run { gitFlow =>
     for {
